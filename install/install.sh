98f11e6b2df955997081b064372a96dfff60cbf5
Douglas Campbell <staplegunn@gmail.com>
2018-02-26 18:39:12 +1300

    Added java to install script

--- install/install.sh
+++ install/install.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 #
 # NMA API server init script
-# 2018-02-23
+# 2018-02-26
 #
 # usage:
 #
 # > sudo su -
-# > apt install git
+# > apt update
+# NB: "apt upgrade" may require manual input
+# > apt upgrade -y -f
+# > apt install git -y
 # > cd /usr/local
 # > git clone https://github.com/Conal-Tuohy/NMA-API-ETL.git
 # > cd /usr/local/NMA-API-ETL/install
-# > ./install.sh "<hostname>" >> /var/log/nma-api-install.log 2>>&1 &
-# > tail -f /var/log/nma-api-install.log
+# > chmod a+x install.sh
+# NB: insert appropriate server name
+# > ./install.sh "<hostname>"
 # > exit
+#
+# NB: If the script stalls, may need to kill any 'apt' processes before re-running
+# > ps -aux | grep 'apt'
+# > sudo kill -9 <process-id>
+# > sudo rm /var/cache/apt/archives/lock
 #
 HOSTNAME=nma.conaltuohy.com
 CONFIG_DIR=/usr/local/NMA-API-ETL/install/config
 INSTALL_DIR=/tmp/nma-api-install
 #
 date
-apt update
-apt upgrade -y -f
 mkdir $INSTALL_DIR
 chown -R ubuntu:ubuntu /usr/local/NMA-API-ETL
 #
@@ -34,8 +41,15 @@ fi
 hostname $HOSTNAME
 echo -e "127.0.0.1 localhost $HOSTNAME nma\n\n$(cat /etc/hosts)" > /etc/hosts
 #
+# JAVA
+#
+apt install default-jdk -y
+JAVA_HOME=/usr/lib/jvm/default-java
+echo "JAVA_HOME=/usr/lib/jvm/default-java" > /etc/environment
+#
 # APACHE HTTP SERVER
 #
+echo =========== Installing Apache HTTP Server
 apt install apache2 -y
 a2enmod proxy_http
 mv /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.original
@@ -43,10 +57,12 @@ ln -s $CONFIG_DIR/apache/000-default.conf /etc/apache2/sites-available/
 #
 # TOMCAT
 #
+echo =========== Installing tomcat
 apt install tomcat8 -y
 #
 # SOLR
 #
+echo =========== Installing Solr
 cd $INSTALL_DIR
 wget http://apache.mirror.digitalpacific.com.au/lucene/solr/7.2.1/solr-7.2.1.tgz -O solr-7.2.1.tgz
 tar xzf solr-7.2.1.tgz solr-7.2.1/bin/install_solr_service.sh --strip-components=2
@@ -67,6 +83,7 @@ chown -R solr:solr $CONFIG_DIR/solr
 #
 # FUSEKI
 #
+echo =========== Installing Fuzeki
 mkdir -p /etc/fuseki/configuration
 ln -s $CONFIG_DIR/fuseki/public.ttl /etc/fuseki/configuration/
 chown -R tomcat8:tomcat8 /etc/fuseki/
@@ -78,6 +95,7 @@ unzip -j fuseki-3.6.0.zip apache-jena-fuseki-3.6.0/fuseki.war -d /var/lib/tomcat
 # XML Calabash
 # - it may be better to just download the zip and unpack it in the appropriate place? (the installer uses /opt/xmlcalabash-blah-blah-version-number)
 #
+echo =========== Installing XML Calabash
 cd $INSTALL_DIR
 wget https://github.com/ndw/xmlcalabash1/releases/download/1.1.16-98/xmlcalabash-1.1.16-98.jar -O xmlcalabash-1.1.16-98.jar
 echo 1 | java -jar ./xmlcalabash-1.1.16-98.jar
@@ -85,6 +103,7 @@ ln -s /usr/local/xmlcalabash-1.1.16-98 /usr/local/xmlcalabash
 #
 # XPROC-Z (ETL)
 #
+echo =========== Installing Xproc-Z ETL
 cd $INSTALL_DIR
 wget https://github.com/Conal-Tuohy/XProc-Z/releases/download/1.0c/xproc-z.war -O xproc-z.war
 mv xproc-z.war /var/lib/tomcat8/webapps/
@@ -92,6 +111,7 @@ ln -s $CONFIG_DIR/tomcat/xproc-z.xml /var/lib/tomcat8/conf/Catalina/localhost/
 #
 # XPROC-Z (API SHIM)
 #
+echo =========== Installing Xproc-Z API shim
 mkdir /etc/xproc-z/
 chmod a+w /etc/xproc-z/
 cp /var/lib/tomcat8/webapps/xproc-z.war /etc/xproc-z/
@@ -100,6 +120,7 @@ git clone https://github.com/Conal-Tuohy/NMA-API.git
 #
 # REFRESH
 #
+echo =========== Restarting services
 service apache2 restart
 service tomcat8 restart
 service solr restart
@@ -110,4 +131,5 @@ service solr restart
 #chmod a+w /data
 #ln -s $CONFIG_DIR/data/Emu_* /data/
 #
+echo =========== API server install complete
 date
