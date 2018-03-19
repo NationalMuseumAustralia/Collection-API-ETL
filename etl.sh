6766217bca94c45b05a91aaf0ec958d32c48f786
Conal Tuohy <conal.tuohy@gmail.com>
2018-03-19 05:18:16 +0000

    save ETL log

--- etl.sh
+++ etl.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-time java -Xmx3G -jar /usr/local/xmlcalabash/xmlcalabash-1.1.16-98.jar etl.xpl
+sudo java -Xmx3G -jar /usr/local/xmlcalabash/xmlcalabash-1.1.16-98.jar etl.xpl > /var/log/NMA-API-ETL/etl.log 2>&1 &
