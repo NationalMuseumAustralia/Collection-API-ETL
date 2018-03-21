86a9f284b19a27353f44d4f167463d6484dcb094
Douglas Campbell <staplegunn@gmail.com>
2018-03-22 12:58:19 +1300

    Add basic unit test harness

---
+++ test/unit/run-unit-tests.bat
@@ -0,0 +1,12 @@
+REM
+REM Run unit tests using XSpec within XProc
+
+set CALABASH_HOME=C:/project/lib/xmlcalabash-1.1.16-98
+set XSPEC_HOME=file:///C:/project/lib/xspec/
+set XSPEC_DATA_DIR_IN=file:///C:/Project/Git/NMA/NMA-API-ETL/test
+set XSPEC_DATA_DIR_OUT=file:///C:/Project/Stage/NMA-unit-test-output
+
+REM EMu to CRM conversion unit tests
+set XSPEC_IN="%XSPEC_DATA_DIR_IN%"/unit/tests/emu-to-crm.xspec
+set XSPEC_OUT="%XSPEC_DATA_DIR_OUT%"/unit/tests/emu-to-crm-test-result.html
+java -cp "%CALABASH_HOME%"/xmlcalabash-1.1.16-98.jar com.xmlcalabash.drivers.Main -i source="%XSPEC_IN%" -p xspec-home="%XSPEC_HOME%" -o result="%XSPEC_OUT%" "%XSPEC_HOME%"/src/harnesses/saxon/saxon-xslt-harness.xproc 
---
+++ test/unit/run-unit-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Run unit tests using XSpec within XProc
+
+CALABASH_HOME=/usr/local/xmlcalabash
+XSPEC_HOME=/usr/local/xspec/
+XSPEC_DATA_DIR_IN=/usr/local/NMA-API-ETL/test
+XSPEC_DATA_DIR_OUT=/usr/local/NMA/tests
+
+# EMu to CRM conversion unit tests
+set XSPEC_IN="$XSPEC_DATA_DIR_IN"/unit/tests/emu-to-crm.xspec
+set XSPEC_OUT="$XSPEC_DATA_DIR_OUT"/unit/tests/emu-to-crm-test-result.html
+java -cp "$CALABASH_HOME"/xmlcalabash-1.1.16-98.jar com.xmlcalabash.drivers.Main -i source="$XSPEC_IN" -p xspec-home="$XSPEC_HOME" -o result="$XSPEC_OUT" "$XSPEC_HOME"/src/harnesses/saxon/saxon-xslt-harness.xproc 
