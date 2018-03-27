259529971d617796fa2219eb82448c9851d5b4f4
Conal Tuohy <conal.tuohy conal.tuohy@gmail.com>
2018-03-27 07:32:48 +0000

    tweaks to XProc launching scripts

--- etl.sh
+++ etl.sh
@@ -1,1 +1,1 @@
-sudo java -Xmx3G -jar /usr/local/xmlcalabash/xmlcalabash-1.1.16-98.jar etl.xpl > /var/log/NMA-API-ETL/etl.log 2>&1 &
+sudo java -Xmx12G -jar /usr/local/xmlcalabash/xmlcalabash-1.1.16-98.jar etl.xpl > /var/log/NMA-API-ETL/etl.log 2>&1 &
