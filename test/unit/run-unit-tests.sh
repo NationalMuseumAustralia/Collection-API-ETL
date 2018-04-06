76f20cc0f239b3b4fc3eab66015df6b546e99ba2
Douglas Campbell <staplegunn@gmail.com>
2018-04-06 17:29:27 +1200

    Add initial unit tests for trix conversions and separate out function tests

--- test/unit/run-unit-tests.sh
+++ test/unit/run-unit-tests.sh
@@ -9,12 +9,22 @@ XSPEC_DATA_DIR_OUT=/usr/local/NMA/test-results
 # Create unit test output director
 mkdir -p "$XSPEC_DATA_DIR_OUT"/unit/tests
 
+# Date util functions unit tests
+XSPEC_IN="$XSPEC_DATA_DIR_IN"/unit/tests/date-util-functions.xspec
+XSPEC_OUT="$XSPEC_DATA_DIR_OUT"/unit/tests/date-util-test-result.html
+java -cp "$CALABASH_HOME"/xmlcalabash-1.1.16-98.jar com.xmlcalabash.drivers.Main -i source="$XSPEC_IN" -p xspec-home="$XSPEC_HOME" -o result="$XSPEC_OUT" "$XSPEC_HOME"/src/harnesses/saxon/saxon-xslt-harness.xproc 
+
+# xml-to-json functions unit tests
+XSPEC_IN="$XSPEC_DATA_DIR_IN"/unit/tests/xmljson-functions.xspec
+XSPEC_OUT="$XSPEC_DATA_DIR_OUT"/unit/tests/xmljson-test-result.html
+java -cp "$CALABASH_HOME"/xmlcalabash-1.1.16-98.jar com.xmlcalabash.drivers.Main -i source="$XSPEC_IN" -p xspec-home="$XSPEC_HOME" -o result="$XSPEC_OUT" "$XSPEC_HOME"/src/harnesses/saxon/saxon-xslt-harness.xproc 
+
 # EMu to CRM conversion unit tests
 XSPEC_IN="$XSPEC_DATA_DIR_IN"/unit/tests/emu-to-crm.xspec
 XSPEC_OUT="$XSPEC_DATA_DIR_OUT"/unit/tests/emu-to-crm-test-result.html
 java -cp "$CALABASH_HOME"/xmlcalabash-1.1.16-98.jar com.xmlcalabash.drivers.Main -i source="$XSPEC_IN" -p xspec-home="$XSPEC_HOME" -o result="$XSPEC_OUT" "$XSPEC_HOME"/src/harnesses/saxon/saxon-xslt-harness.xproc 
 
-REM Trix to DC conversion unit tests
+# Trix to DC conversion unit tests
 XSPEC_IN="$XSPEC_DATA_DIR_IN"/unit/tests/trix-description-to-dc.xspec
 XSPEC_OUT="$XSPEC_DATA_DIR_OUT"/unit/tests/trix-to-dc-test-result.html
 java -cp "$CALABASH_HOME"/xmlcalabash-1.1.16-98.jar com.xmlcalabash.drivers.Main -i source="$XSPEC_IN" -p xspec-home="$XSPEC_HOME" -o result="$XSPEC_OUT" "$XSPEC_HOME"/src/harnesses/saxon/saxon-xslt-harness.xproc 
