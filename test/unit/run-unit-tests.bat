82414c65e467fd8be9617e4e0d52d717c77d9677
Douglas Campbell <staplegunn@gmail.com>
2018-04-14 02:01:04 +1200

    Add interim handling of multiple DC representation levels for media endpoint conversion

--- test/unit/run-unit-tests.bat
+++ test/unit/run-unit-tests.bat
@@ -25,12 +25,17 @@ set XSPEC_IN="%XSPEC_DATA_DIR_IN%"/unit/tests/emu-to-crm.xspec
 set XSPEC_OUT="%XSPEC_DATA_DIR_OUT%"/unit/tests/emu-to-crm-test-result.html
 java -cp "%CALABASH_HOME%"/xmlcalabash-1.1.16-98.jar com.xmlcalabash.drivers.Main -i source="%XSPEC_IN%" -p xspec-home="%XSPEC_HOME%" -o result="%XSPEC_OUT%" "%XSPEC_HOME%"/src/harnesses/saxon/saxon-xslt-harness.xproc 
 
-REM Trix to DC conversion unit tests
-set XSPEC_IN="%XSPEC_DATA_DIR_IN%"/unit/tests/trix-description-to-dc.xspec
-set XSPEC_OUT="%XSPEC_DATA_DIR_OUT%"/unit/tests/trix-to-dc-test-result.html
+REM Trix to DC (object) conversion unit tests
+set XSPEC_IN="%XSPEC_DATA_DIR_IN%"/unit/tests/trix-description-to-dc-object.xspec
+set XSPEC_OUT="%XSPEC_DATA_DIR_OUT%"/unit/tests/trix-to-dc-object-test-result.html
 java -cp "%CALABASH_HOME%"/xmlcalabash-1.1.16-98.jar com.xmlcalabash.drivers.Main -i source="%XSPEC_IN%" -p xspec-home="%XSPEC_HOME%" -o result="%XSPEC_OUT%" "%XSPEC_HOME%"/src/harnesses/saxon/saxon-xslt-harness.xproc 
 
-REM Trix to Solr conversion unit tests
-set XSPEC_IN="%XSPEC_DATA_DIR_IN%"/unit/tests/trix-description-to-solr.xspec
-set XSPEC_OUT="%XSPEC_DATA_DIR_OUT%"/unit/tests/trix-to-solr-test-result.html
+REM Trix to DC (media) conversion unit tests
+set XSPEC_IN="%XSPEC_DATA_DIR_IN%"/unit/tests/trix-description-to-dc-media.xspec
+set XSPEC_OUT="%XSPEC_DATA_DIR_OUT%"/unit/tests/trix-to-dc-media-test-result.html
+java -cp "%CALABASH_HOME%"/xmlcalabash-1.1.16-98.jar com.xmlcalabash.drivers.Main -i source="%XSPEC_IN%" -p xspec-home="%XSPEC_HOME%" -o result="%XSPEC_OUT%" "%XSPEC_HOME%"/src/harnesses/saxon/saxon-xslt-harness.xproc 
+
+REM Trix to Solr (object) conversion unit tests
+set XSPEC_IN="%XSPEC_DATA_DIR_IN%"/unit/tests/trix-description-to-solr-object.xspec
+set XSPEC_OUT="%XSPEC_DATA_DIR_OUT%"/unit/tests/trix-to-solr-object-test-result.html
 java -cp "%CALABASH_HOME%"/xmlcalabash-1.1.16-98.jar com.xmlcalabash.drivers.Main -i source="%XSPEC_IN%" -p xspec-home="%XSPEC_HOME%" -o result="%XSPEC_OUT%" "%XSPEC_HOME%"/src/harnesses/saxon/saxon-xslt-harness.xproc 
