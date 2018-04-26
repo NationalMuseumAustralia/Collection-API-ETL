381db6bc61b881c9767c8d0d752eeb61020fc60c
root <root@nma-dev.conaltuohy.com>
2018-04-26 01:45:03 +0000

    Add parms to extract trix script call

--- util/extract-trix.sh
+++ util/extract-trix.sh
@@ -1,1 +1,1 @@
-time java -Xmx1G -jar /usr/local/xmlcalabash/xmlcalabash-1.1.16-98.jar extract-trix.xpl > /var/log/NMA-API-ETL/extract-trix.log 2>&1 &
+time java -Xmx1G -jar /usr/local/xmlcalabash/xmlcalabash-1.1.16-98.jar extract-trix.xpl type="$1" id="$2" > /var/log/NMA-API-ETL/extract-trix.log 2>&1 &
