53f0cbf295403bb0d8112781dda3a553c97ec9e0
Conal Tuohy <conal.tuohy@gmail.com>
2018-08-22 19:32:20 +1000

    direct more messages to main 'etl-to-fuseki' log

--- etl-to-fuseki-full.sh
+++ etl-to-fuseki-full.sh
@@ -19,29 +19,31 @@ cd /usr/local/NMA-API-ETL
 java -Xmx4G -jar /usr/local/xmlcalabash/xmlcalabash.jar etl-to-fuseki.xpl incremental="false" dataset="$DATASET" > "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
 
 # stop fuseki in order to rebuild its tdb2 database
-sudo java -Xmx1G -jar /usr/local/xmlcalabash/xmlcalabash.jar manage-tomcat.xpl command="stop?path=/fuseki"
+echo Stopping fuseki... >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
+sudo java -Xmx1G -jar /usr/local/xmlcalabash/xmlcalabash.jar manage-tomcat.xpl command="stop?path=/fuseki" >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
 sleep 5
 # delete fuseki's lock files which why doesn't fuseki delete it itself?
 rm "/etc/fuseki/databases/$DATASET/tdb.lock"
 # delete fuseki's datasets
-rm -r -f "/etc/fuseki/databases/$DATASET/*"
+echo Deleting existing fuseki dataset files ... >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
+rm -r -f "/etc/fuseki/databases/$DATASET/*" >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
 
 # assemble all the nquads files (which contain single graphs) into a single nquads file containing the entire RDF dataset
 # first delete any old dataset files which may have been left lying around
 rm "/data/$DATASET/dataset.nq"
 
 # concatenate n-quads files into new dataset file
-find "/data/$DATASET/n-quads/" -name "*.nq" | xargs cat >> "/data/$DATASET/dataset.nq"
+find "/data/$DATASET/n-quads/" -name "*.nq" | xargs cat >> "/data/$DATASET/dataset.nq" >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
 
 # now remove the individual .nq files 
-find "/data/$DATASET/n-quads/" -name "*.nq" -delete
+find "/data/$DATASET/n-quads/" -name "*.nq" -delete >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
 
 # rebuild the fuseki db from the dataset files
-echo Building Fuseki $DATASET dataset ...
-time sudo -u tomcat8 /usr/local/jena/bin/tdb2.tdbloader --tdb="/etc/fuseki/configuration/$DATASET.ttl" "/data/$DATASET/dataset.nq" >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log"
+echo Building Fuseki $DATASET dataset ... >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
+time sudo -u tomcat8 /usr/local/jena/bin/tdb2.tdbloader --tdb="/etc/fuseki/configuration/$DATASET.ttl" "/data/$DATASET/dataset.nq" >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
 
 # generate triple pattern statistics for Fuseki query optimizer
-echo Generating triple pattern statistics for $DATASET dataset ...
+echo Generating triple pattern statistics for $DATASET dataset ... >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
 sudo -u tomcat8 /usr/local/jena/bin/tdb2.tdbstats --graph urn:x-arq:UnionGraph --loc "/etc/fuseki/databases/$DATASET" > "/tmp/stats-$DATASET.opt"
 sudo -u tomcat8 cp "/tmp/stats-$DATASET.opt" "/etc/fuseki/databases/$DATASET/"
 
@@ -49,4 +51,5 @@ sudo -u tomcat8 cp "/tmp/stats-$DATASET.opt" "/etc/fuseki/databases/$DATASET/"
 rm "/data/$DATASET/dataset.nq"
 
 # restart fuseki server 
-sudo java -Xmx1G -jar /usr/local/xmlcalabash/xmlcalabash.jar manage-tomcat.xpl command="start?path=/fuseki"
+echo Restarting fuseki ... >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
+sudo java -Xmx1G -jar /usr/local/xmlcalabash/xmlcalabash.jar manage-tomcat.xpl command="start?path=/fuseki" >> "/var/log/NMA-API-ETL/etl-to-fuseki-$DATASET.log" 2>&1
