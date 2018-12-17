d90b8bb87f292c9d05fc58d3653e27b0d2c4ee0b
Conal Tuohy <conal.tuohy@gmail.com>
2018-12-17 15:51:46 +1100

    include hostname in sender address of ETL completion email

--- etl-run-all.sh
+++ etl-run-all.sh
@@ -148,5 +148,6 @@ mkdir $OUT_DIR/failed-solr-deposits
 mv /data/failed-solr-deposits/* $OUT_DIR/failed-solr-deposits/
 # NB: don't need to copy etl-run-all.log as is already in the output dir
 to_log "END ETL - mode=$MODE, job=$JOB_ID"
-sendemail -f '"NMA ETL" no_reply@data.nma.gov.au' -t conal.tuohy@gmail.com -u 'NMA ETL results' -m "See attached log file." -a $OUT_DIR/etl-run-all.log
+HOSTNAME=`hostname`
+sendemail -f "$HOSTNAME ETL <no_reply@$HOSTNAME>" -t conal.tuohy@gmail.com -u 'NMA ETL results' -m "See attached log file." -a $OUT_DIR/etl-run-all.log
 exit 0
